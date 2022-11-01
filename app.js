8ac52f6643ffa38f9278085a72351960e5089fdc
Adi <valcuadrian90@yahoo.com>
2022-11-01 17:07:25 +0200

    mods1

--- app.js
+++ app.js
@@ -26,8 +26,11 @@ function getRandomPhraseAsArray(a) {
     return index;
 };
 
+
 const randomPhrase = getRandomPhraseAsArray(phrases);
 console.log(randomPhrase);
+
+
 
 //ADD PHRASE TO DISPLAY
 function addPhraseToDisplay() {
@@ -47,20 +50,38 @@ addPhraseToDisplay();
 
 //CHECK LETTER
 function checkLetter(event) {
-    console.log('event', event);
+    console.log(event.target);
     const lettersToCheck = document.querySelectorAll('ul li.letter');
     console.log(lettersToCheck);
-    const match = null;
-    for (let i=0; i < lettersToCheck.length; i++) {
-        if (lettersToCheck.includes("")) {
-            li.className = 'show';
+    const letter = event.target.innerHTML;
+    console.log(letter);
+
+    // console.log(lettersToCheck);
+
+    const match = '';
+    for (let i = 0; i < buttons.length; i++) {
+        console.log(lettersToCheck[i]);
+        if (lettersToCheck[i].innerHTML === letter) {
+            
         } 
     }
+    // return match;
 }
 
 buttons.forEach((e) => {
-    console.log(e);
     e.addEventListener('click', checkLetter);    
 });
+
+function letterToLi(){
+    const string = randomPhrase.split(' ').join('');
+    const list = document.querySelectorAll('ul li.letter');
+    console.log(list);
+    for ( let i = 0; i < list.length; i++) {
+        list.innerHTML = string[i];
+        
+    }
+}
+
+letterToLi();
 
 
